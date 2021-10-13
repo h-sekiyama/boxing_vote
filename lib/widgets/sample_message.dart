1e7b62866bc9b1ecb84d81bda64e77191648e92a
hidemitsu sekiyama <hidemitsu.sekiyama@gmail.com>
2021-10-14 01:55:58 +0900

    FireStoreからデータ取得

--- lib/widgets/sample_message.dart
+++ lib/widgets/sample_message.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class SampleMessage extends StatelessWidget {
+class SampleMessage extends StatefulWidget {
+  @override
+  _MyFirestorePageState createState() => _MyFirestorePageState();
+}
+
+class _MyFirestorePageState extends State<SampleMessage> {
+  // ドキュメント情報を入れる箱を用意
+  List<DocumentSnapshot> documentList = [];
+
+  void initState() {
+    fetchBoutData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello, World"),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            // ドキュメント情報を表示
+            Column(
+              children: documentList.map((document) {
+                return ListTile(
+                  title: Text('興行名:${document['event_name']}'),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void fetchBoutData() async {
+    // 指定コレクションのドキュメント一覧を取得
+    final snapshot = await FirebaseFirestore.instance.collection('bouts').get();
+    // ドキュメント一覧を配列で格納
+    setState(() {
+      documentList = snapshot.docs;
+    });
   }
 }

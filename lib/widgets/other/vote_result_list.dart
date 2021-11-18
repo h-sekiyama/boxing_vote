5b283548ca03777a4483ef3a0ff349e6ac921117
hidemitsu sekiyama <hidemitsu.sekiyama@gmail.com>
2021-11-19 01:13:54 +0900

    予想結果一覧デザイン修正

--- lib/widgets/other/vote_result_list.dart
+++ lib/widgets/other/vote_result_list.dart
@@ -57,10 +57,14 @@ class _MyFirestorePageState extends State<VoteResultList> {
             Column(children: [
               ListTile(
                   leading: _displaySelectionImageOrGrayImage(),
-                  title: Text('${userName}'),
+                  title: Text('${userName}',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
                   subtitle: Text('${title}')),
               Text(
-                  '全予想試合数：${totalVoteBoutCount}\n集計済み試合数：${endTotalVoteBoutCount}\n的中試合数：${wonBoutCount}\n的中率：${wonBoutRate.toStringAsFixed(1)}%')
+                  '全予想試合数：${totalVoteBoutCount}\n集計済み試合数：${endTotalVoteBoutCount}\n的中試合数：${wonBoutCount}\n的中率：${wonBoutRate.toStringAsFixed(1)}%',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ))
             ]),
             // ドキュメント情報を表示
             Expanded(
@@ -106,53 +110,131 @@ class _MyFirestorePageState extends State<VoteResultList> {
                   resultText = "引き分けまたは無効試合";
                 }
                 return Visibility(
-                    // 投票数が10件以上で、間違い情報がそれより多い試合情報は表示しない
-                    visible: document["wrong_info_count"] < totalVotedCount ||
-                        totalVotedCount < 10,
-                    child: Card(
-                      child: Column(children: [
-                        ListTile(
-                          title: Text(
-                              '${document['fighter1']} VS ${document['fighter2']}'),
-                          subtitle: Text('${document['event_name']}'),
-                          trailing: Text(Functions.dateToString(fight_date)),
+                  // 投票数が10件以上で、間違い情報がそれより多い試合情報は表示しない
+                  visible: document["wrong_info_count"] < totalVotedCount ||
+                      totalVotedCount < 10,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Column(children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(4, 12, 4, 6),
+                        child: Text(
+                            Functions.dateToString(fight_date) +
+                                " / " +
+                                document['event_name'],
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(4, 8, 4, 10),
+                        width: MediaQuery.of(context).size.width * 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Flexible(
+                                child: Text(document['fighter1'],
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 24,
+                                        color: Color(0xffFF4B4B)))),
+                            Container(
+                                width: 50,
+                                child: Text('VS',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold))),
+                            Flexible(
+                                child: Text(document['fighter2'],
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 24,
+                                        color: Color(0xff4B9EFF)))),
+                          ],
                         ),
-                        ListTile(
-                            title: Text(
-                                '${document['fighter1']}の勝ち予想：${document['vote1'] + document['vote2']}人\n${document['fighter2']}の勝ち予想：${document['vote3'] + document['vote4']}人',
-                                style: TextStyle(fontSize: 14.0))),
-                        // 予想結果
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("予想："),
-                              Text(votedText,
-                                  style: TextStyle(color: HexColor('ff0000'))),
-                            ]),
-                        // 試合結果
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("結果："),
-                              Text(resultText,
-                                  style: TextStyle(color: HexColor('ff0000'))),
-                            ]),
-                        ElevatedButton(
-                          child: const Text('この試合について語る'),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.orange,
-                            onPrimary: Colors.white,
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => ChatScreen(document.id),
-                                ));
-                          },
-                        ),
-                      ]),
-                    ));
+                      ),
+                      Container(
+                          width: MediaQuery.of(context).size.width * 0.85,
+                          height: 8,
+                          margin: EdgeInsets.fromLTRB(0, 12, 4, 4),
+                          child: LinearProgressIndicator(
+                            value: document['vote1']!.toDouble() +
+                                        document['vote2']!.toDouble() +
+                                        document['vote3']!.toDouble() +
+                                        document['vote4']!.toDouble() !=
+                                    0
+                                ? (document['vote1']!.toDouble() +
+                                        document['vote2']!.toDouble()) /
+                                    (document['vote1']!.toDouble() +
+                                        document['vote2']!.toDouble() +
+                                        document['vote3']!.toDouble() +
+                                        document['vote4']!.toDouble())
+                                : 0,
+                            backgroundColor: Color(0xff4B9EFF),
+                            valueColor:
+                                new AlwaysStoppedAnimation<Color>(Colors.red),
+                          )),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(left: 14),
+                              child: Text(
+                                  (document['vote1']! + document['vote2']!)
+                                          .toString() +
+                                      "人勝ち予想",
+                                  textAlign: TextAlign.left)),
+                          Container(
+                              margin: EdgeInsets.only(right: 14),
+                              child: Text(
+                                  (document['vote3']! + document['vote4']!)
+                                          .toString() +
+                                      "人勝ち予想",
+                                  textAlign: TextAlign.right))
+                        ],
+                      ),
+                      // 予想結果
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("予想：" + votedText,
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                          ]),
+                      // 試合結果
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("結果：" + resultText,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xffFF7A00))),
+                          ]),
+                      Container(
+                          width: 187,
+                          height: 38,
+                          margin: EdgeInsets.fromLTRB(0, 8, 0, 30),
+                          child: (TextButton(
+                            child: const Text('この試合について語る',
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            style: ElevatedButton.styleFrom(
+                              primary: HexColor('000000'),
+                              onPrimary: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        ChatScreen(document.id),
+                                  ));
+                            },
+                          ))),
+                    ]),
+                  ),
+                );
               }).toList(),
             )),
           ],
@@ -228,7 +310,7 @@ class _MyFirestorePageState extends State<VoteResultList> {
   Widget _displaySelectionImageOrGrayImage() {
     if (nowImage != null) {
       return Container(
-        width: 80,
+        width: 60,
         height: 80,
         child: ClipRRect(
           child: nowImage,
@@ -236,7 +318,7 @@ class _MyFirestorePageState extends State<VoteResultList> {
       );
     } else {
       return Container(
-        width: 80,
+        width: 60,
         height: 80,
         child: ClipRRect(
           child: Image.asset('images/cat.png'),
